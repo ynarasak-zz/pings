ab114bec7712497097e73d0d53b1a62b20d2a8d2
narasaki <mduckfrogbby@gmail.com>
2018-11-01 15:33:42 +0900

    second commit

--- app/workers/ping_worker.rb
+++ app/workers/ping_worker.rb
@@ -6,10 +6,10 @@ class PingWorker
 
   def perform(*args)
     # do something
-    u = User.new
-    u.name = (0...8).map{ (65 + rand(26)).chr }.join
-    u.save
-    uri = URI.parse("https://nara-sample.herokuapp.com/static_pages/archives/2.html")
+    #u = User.new
+    str = (0...8).map{ (65 + rand(26)).chr }.join
+    #u.save
+    uri = URI.parse("https://nara-sample.herokuapp.com/static_pages/archives/"+str)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     p http.get(uri.request_uri)
