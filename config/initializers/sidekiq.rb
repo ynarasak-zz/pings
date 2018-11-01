37b0ebf9f1f4be75458ed9d9f32450103e4c9383
narasaki <mduckfrogbby@gmail.com>
2018-11-01 16:06:12 +0900

    REDIS_URL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,11 @@
 schedule_file = "config/schedule.yml"
 
+Sidekiq.configure_server do |config|
+  if Rails.env.production?
+    config.redis = { url: ENV.fetch('REDIS_URL') }
+  end
+end
+
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
