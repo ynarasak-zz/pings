8b19afe3a0894324724909b8610683873336080b
narasaki <mduckfrogbby@gmail.com>
2018-11-01 16:20:15 +0900

    heroku + sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,6 +5,11 @@ Sidekiq.configure_server do |config|
     config.redis = { url: ENV.fetch('REDIS_URL') }
   end
 end
+Sidekiq.configure_client do |config|
+  if Rails.env.production?
+    config.redis = { url: ENV.fetch('REDIS_URL') }
+  end
+end
 
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
---
+++ config/unicorn.rb
@@ -0,0 +1,33 @@
+timeout 15
+preload_app true
+
+worker_processes 3
+
+# whatever you had in your unicorn.rb file
+@sidekiq_pid = nil
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
+  if Rails.env.production?
+    #@sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+    @sidekiq_pid ||= spawn("bundle exec sidekiq -C config/sidekiq.yml")
+    Rails.logger.info('Spawned sidekiq #{@sidekiq_pid}')
+  end
+end 
+
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
