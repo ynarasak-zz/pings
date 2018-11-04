cc5c595457c7e9a5a601f93121e623ebdc221b7b
narasaki <mduckfrogbby@gmail.com>
2018-11-05 07:55:26 +0900

    hagu

--- app/workers/ping_worker.rb
+++ app/workers/ping_worker.rb
@@ -9,7 +9,8 @@ class PingWorker
     #u = User.new
     str = (0...8).map{ (65 + rand(26)).chr }.join
     #u.save
-    uri = URI.parse("https://nara-sample.herokuapp.com/static_pages/archives/"+str)
+    #uri = URI.parse("https://nara-sample.herokuapp.com/static_pages/archives/"+str)
+    uri = URI.parse("https://hagu-fund.herokuapp.com/companies")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     p http.get(uri.request_uri)
@@ -19,9 +20,9 @@ class PingWorker
     http.use_ssl = true
     p "self -> "+http.get(uri.request_uri)
 
-    uri = URI.parse("http://studio-c25.herokuapp.com/ping.html")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    p "ping -> "+http.get(uri.request_uri)
+    #uri = URI.parse("http://studio-c25.herokuapp.com/ping.html")
+    #http = Net::HTTP.new(uri.host, uri.port)
+    #http.use_ssl = true
+    #p "ping -> "+http.get(uri.request_uri)
   end
 end
